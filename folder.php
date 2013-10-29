40a265fd770dca6b62afb6beb3de46cfb0a4b618
ckay2003 <ckay2003@gmail.com>
2013-10-29 13:51:47 +0100

    folder.php an config angepasst

--- folder.php
+++ folder.php
@@ -12,19 +12,20 @@
 
 </head>		
 <?php
+require_once('config.inc.php');
 $now = date('Y-m-d');
 $now2 = date('H.m.s');
 $now3 = "$now $now2";
 require_once('./getid3/getid3.php');
-mysql_connect("localhost", "root","strese84");
-mysql_select_db("musikdatenbank") or die ("Die Datenbank existiert nicht.");
+mysql_connect(DBHOST, DBUSER,DBPASS) OR DIE ("NICHT Erlaubt");
+mysql_select_db(DBDATABASE) or die ("Die Datenbank existiert nicht.");
 mysql_query("TRUNCATE `scanner_log`");
 mysql_query("INSERT INTO scanner_log (starttime) VALUES ('$now3')");	
 $dirs = array();
 // Initialize getID3 engine
 $getID3 = new getID3;
 
-$DirectoryToScan = '/mnt/musik/Eisregen/*'; // change to whatever directory you want to scan
+$DirectoryToScan = MUSICDIR; // change to whatever directory you want to scan
 
 $out  = '';  
 function scan($folder){  
@@ -34,7 +35,7 @@ function scan($folder){
   if($content = opendir($folder)){  
 
     while(false !== ($file = readdir($content))){  
-      if(is_dir("$folder/$file") && $file != "." && $file != ".." && !in_array($file, $hide)){ 
+      if(is_dir("$folder/$file") && $file != "." && $file != ".."){ 
 
         $out .= "$folder/$file,";   
            scan("$folder/$file");  
@@ -51,7 +52,7 @@ function scan($folder){
 // function end  
 
 
-echo scan('/mnt/musik/Eisregen');
+echo scan($DirectoryToScan);
 $dirs = (explode(',', $out));
 
 $dirs[] = "ende";
