83f0aa8df4762b58a95da891bc88f5b8f8625544
ckay2003 <ckay2003@gmail.com>
2013-10-29 21:44:40 +0100

    info div wieder sichtbar :)

--- artist.php
+++ artist.php
@@ -194,7 +194,7 @@ width: 55%;
 left: 320px;
 min-height: 194px;
 top: 8px;
-display:none;
+display:inline-block;
 -webkit-box-shadow:0 6px 6px 0 rgba(0,0,0,0.3);
 box-shadow:0 2px 6px 0 rgba(0,0,0,0.3);
 background:#fff;
