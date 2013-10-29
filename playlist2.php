5edcd24a6dba7b2740e69d9c273b32d06d50b596
stobiaskoch <s.t.koch77@gmail.com>
2013-10-29 15:37:46 +0100

    Delete playlist2.php

--- playlist2.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Playlist
-$playlist[]=array("artist"=>"AC/DC","title"=>"Thunderstruck","mp3"=>"mp3.php?datei=01 Thunderstruck.mp3");
-$playlist[]=array("artist"=>"AC/DC","title"=>"Shoot To Thrill","mp3"=>"mp3.php?datei=02 Shoot to Thrill.mp3");
-$playlist[]=array("artist"=>"AC/DC","title"=>"Back In Black","mp3"=>"mp3.php?datei=03 Back in Black.mp3");
-$playlist=json_encode($playlist);
-print_r($playlist);
-?>
