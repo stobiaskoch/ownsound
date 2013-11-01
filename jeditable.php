611feaddaea610e00b1ea8cdeb0dbc0be24c2c61
stobiaskoch <s.t.koch77@gmail.com>
2013-11-01 19:08:02 +0100

    Update jeditable.php

--- jeditable.php
+++ jeditable.php
@@ -5,13 +5,10 @@ mysql_select_db(DBDATABASE) or die ("Die Datenbank existiert nicht.");
 
 $id = $_REQUEST['id'];
 $newvalue = $_REQUEST['value'];
-//mysql_query("UPDATE album SET name=$newvalue WHERE id='$id'");
 
 	$sql    = "UPDATE `album` SET name='$newvalue' WHERE id='$id'";
-//		$sql = "INSERT INTO album (name) VALUE ('$newvalue')";
-		$query = mysql_query($sql);
-		echo mysql_error();
-		
+	$query = mysql_query($sql);
+
 echo $newvalue;
 ?>
 
