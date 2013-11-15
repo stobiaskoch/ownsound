a366e1bd561af0c14645cfaedf2c465fbac12cf6
stobiaskoch <s.t.koch77@gmail.com>
2013-11-15 15:12:16 +0100

    Update folder.php

--- folder.php
+++ folder.php
@@ -70,12 +70,13 @@ set_time_limit(3000);
 
 
 
-echo "Starte Scan.... Bitte warten";
+
 require_once('./ownsound.config.php');
 require_once './class.progress_bar.php';
-
+include('./dirscan.php');
 
 $DirectoryToScan = utf8_encode($_REQUEST['scandir']);
+echo "<p style='font-size: 9px;'>Durchsuche $DirectoryToScan .... Bitte warten</p>";
 //echo $DirectoryToScan;
 //$DirectoryToScan = "/mnt/musik/Metallica";
 //if($_REQUEST['scandir']=="") {die();}
@@ -89,28 +90,9 @@ mysql_query("TRUNCATE `title`");
 }
 
 mysql_query("TRUNCATE `scanner`");
-//if (substr($file, -3)==".mp3") 
 
-// Gibt rekursiv alle Ordner, alle Unterordner und Dateien eines Verzeichnisses aus
-function get_dir ($dir) {
-	$fp=opendir($dir);
-	while($datei=readdir($fp)) {
-		set_time_limit(100);
-		if (substr($datei, -3)=="mp3"){
-			$file = utf8_encode($dir."/".$datei);
-			mysql_query("INSERT INTO scanner (path) VALUES ('$file')");
-		}
-		if (is_dir($dir."/".$datei) && $datei!="." && $datei!=".."){
-			get_dir($dir."/".$datei);
-		}
-	}
-	closedir($fp);
-}
-get_dir($DirectoryToScan);
-
-
-
-
+		
+map_dirs($_REQUEST['scandir'],1);
 
 ?>
 
@@ -125,21 +107,12 @@ get_dir($DirectoryToScan);
 
 
 
-
-<?php
-
-$scanchecker = $checkscan / 20;
-
-
-
-//$num_tasks = $scanchecker; // the number of tasks to be completed. 
-	?>
 	<script language="JavaScript">
 	start_task();
 	</script>
 
 
-<div id="results" style="border:1px solid #000; padding:10px; width:300px; height:15px; overflow:auto; background:#eee; z-index:10000;"></div>
+<div id="results" style="font-size: 9px; border:1px solid #000; padding:10px; width:300px; height:15px; overflow:auto; background:#eee; z-index:10000;"></div>
         <br />
          
         <div style="border:1px solid #ccc; width:300px; height:20px; overflow:auto; background:#eee;">
