a895a1a508b7b6a5af42af55b8efdb64d1923d6a
stobiaskoch <s.t.koch77@gmail.com>
2013-11-25 11:18:09 +0100

    sucher verbessert

--- rpc.php
+++ rpc.php
@@ -3,14 +3,18 @@
 	mysql_connect(DBHOST, DBUSER,DBPASS) OR DIE ("NICHT Erlaubt");
 	mysql_select_db(DBDATABASE) or die ("Die Datenbank existiert nicht.");
 	mysql_query("SET NAMES 'utf8'");
+	include('./js/functions.php');
 	
 	$result = mysql_query("SELECT * FROM artist WHERE name LIKE '%" . $_GET['search'] . "%' LIMIT 5");
 	while($row = mysql_fetch_object($result))
 	{
 		echo '';
+		$trackcount = trackcount($row->id);
+		if($trackcount>=1) {
 		?> 
-		<a style="font-size: 13px;" href='#OwnSound' onclick="getdata('<?php echo $row->id; ?>')"><?php echo $row->name; ?> [Artist]</a><br>
+		<a style="font-size: 12px;" href='#OwnSound' onclick="getdata('<?php echo $row->id; ?>')"><?php echo $row->name; ?> [Artist]</a><br>
 		<?php
+		}
 		echo '';
 	}
 echo '<br>';
@@ -20,10 +24,10 @@ echo '<br>';
 	{
 		echo '';
 		?>
-		<a style="font-size: 13px;" href='#OwnSound' onclick="
+		<a style="font-size: 12px;" href='#OwnSound' onclick="
 										getdataalbum('<?php echo $row->id; ?>', '<?php echo $row->artist; ?>');
 										getdata('<?php echo $row->artist; ?>');">
-										<?php echo $row->name; ?> [Album]</a><br>
+										<?php echo getartist($row->artist)." - ". $row->name; ?> [Album]</a><br>
 		<?php
 		echo '';
 	}
