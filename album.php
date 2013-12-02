8103c6cd71c34301ee16719605a061d1ee8aba48
stobiaskoch <s.t.koch77@gmail.com>
2013-12-02 07:55:13 +0100

    10albumfix

--- album.php
+++ album.php
@@ -119,7 +119,7 @@ coverjump:
 	?>		
 		
 		<li id="Coverflow-1" title="<?php echo getartist($artistID); ?>" data-flip-category="<?php echo getalbum($albumID); ?>">
-			<a href='#OwnSound' onclick="getdataalbum('<?php echo $albumID; ?>', '<?php echo $artistID; ?>')">
+			<a href='#OwnSound' onclick="getdataalbum('<?php echo $albumID; ?>', '<?php echo getartistIDfromalbumID($albumID); ?>')">
 			<img src='get.php?picid=<?php echo $albumID; ?>' title="<?php echo getalbum($albumID); ?>" width="140" height="140">
 		</li>
 
